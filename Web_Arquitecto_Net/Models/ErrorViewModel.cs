22858759d4cb6d53e23ca852065907f919787f2a
CarlosCode502 <carloshl.code21@gmail.com>
2023-11-01 16:58:14 -0600

    Se creó el proyecto:
    Sitio web para arquitectos en .Net MVC.

---
+++ Web_Arquitecto_Net/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web_Arquitecto_Net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
